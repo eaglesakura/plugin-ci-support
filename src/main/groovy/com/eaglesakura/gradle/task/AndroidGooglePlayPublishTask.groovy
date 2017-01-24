8f70c60c8e857f04cb77b71b957c552b33ec156f
eaglesakura <eagle.sakura@gmail.com>
2017-01-24 10:10:34 +0900

    Google Play配信Taskを追加

---
+++ src/main/groovy/com/eaglesakura/gradle/task/AndroidGooglePlayPublishTask.groovy
@@ -0,0 +1,73 @@
+package com.eaglesakura.gradle.task
+
+import com.eaglesakura.gradle.googleplay.GooglePlayConsoleManager
+import com.eaglesakura.tool.log.Logger
+import com.eaglesakura.util.IOUtil
+import com.eaglesakura.util.StringUtil
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+/**
+ * Google PlayへのアップロードをサポートするServiceを提供する
+ *
+ * アップロードは必ずServiceAccountを通して行う。
+ */
+public class AndroidGooglePlayPublishTask extends DefaultTask {
+
+    File p12;
+
+    File apk;
+
+    String applicationId;
+
+    String serviceAccountEmail;
+
+    String track;
+
+    /**
+     * listings
+     *   * ja-JP/title.txt
+     *   * ja-JP/fullDescription.txt
+     *   * ja-JP/shortDescription.txt
+     */
+    File listings;
+
+    /**
+     * apk update Listings
+     *  * ja-JP/apk.txt
+     */
+    File apkListings;
+
+
+    protected GooglePlayConsoleManager googlePlayConsoleManager;
+
+    protected void onGooglePayTask() {
+        googlePlayConsoleManager.autholize();
+
+        if (!StringUtil.isEmpty(track)) {
+            googlePlayConsoleManager.uploadApk(track, apkListings);
+        }
+
+        if (IOUtil.isDirectory(listings)) {
+            googlePlayConsoleManager.updateListings(listings);
+        }
+    }
+
+    @TaskAction
+    def onExecute() {
+        Logger.out "applicationId       : ${applicationId}"
+        Logger.out "track               : ${track}"
+        Logger.out "listings            : ${listings}"
+        Logger.out "apk                 : ${apk}"
+        Logger.out "apklistings         : ${apkListings}"
+        Logger.out "serviceAccountEmail : ${serviceAccountEmail}"
+
+        googlePlayConsoleManager = new GooglePlayConsoleManager();
+        googlePlayConsoleManager.p12 = p12;
+        googlePlayConsoleManager.apk = apk;
+        googlePlayConsoleManager.applicationId = applicationId;
+        googlePlayConsoleManager.serviceAccountEmail = serviceAccountEmail;
+
+        onGooglePayTask();
+    }
+}
